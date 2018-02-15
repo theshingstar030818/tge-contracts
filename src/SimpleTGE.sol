f31006ec093e6facdda603836fa43336dfcc544c
vignesh sundaresan <vignesh@lendroid.com>
2018-02-14 21:59:14 -0500

    [second pass audit recommendations] removed whitelist as independent contract and moved the logic inside simpleTGE whitelistAddresses, blacklistAddresses funcs added singular functions removed Avoid local variables in contribute functions Added isWhitelisted modifier at appropriate places and vest function now has more validation

--- src/SimpleTGE.sol
+++ src/SimpleTGE.sol
@@ -76,125 +76,6 @@ contract Ownable {
 }
 
 
- contract PausableDestructible is Ownable{
-
-   event Pause();
-   event Unpause();
-
-   bool public paused = false;
-
-   /**
-    * @dev Modifier to make a function callable only when the contract is not paused.
-    */
-   modifier whenNotPaused() {
-     require(!paused);
-     _;
-   }
-
-   /**
-    * @dev Modifier to make a function callable only when the contract is paused.
-    */
-   modifier whenPaused() {
-     require(paused);
-     _;
-   }
-
-   /**
-    * @dev called by the owner to pause, triggers stopped state
-    */
-   function pause() onlyOwner whenNotPaused external {
-     paused = true;
-     Pause();
-   }
-
-   /**
-    * @dev called by the owner to unpause, returns to normal state
-    */
-   function unpause() onlyOwner whenPaused external {
-     paused = false;
-     Unpause();
-   }
-
-   /**
-    * @dev Transfers the current balance to the owner and terminates the contract.
-    */
-   function reclaimEtherOrDestroy(bool _destroy) external onlyOwner whenPaused returns(bool) {
-     if (_destroy) {
-       selfdestruct(owner);
-       return true;
-     }
-     /* else {
-       owner.transfer(this.balance);
-       return true;
-     } */
-     return false;
-   }
-
- }
-
-
-
-
-contract PausableDestructibleHasNoEther is PausableDestructible {
-
-   /**
-   * @dev Constructor that rejects incoming Ether
-   * @dev The `payable` flag is added so we can access `msg.value` without compiler warning. If we
-   * leave out payable, then Solidity will allow inheriting contracts to implement a payable
-   * constructor. By doing it this way we prevent a payable constructor from working. Alternatively
-   * we could use assembly to access msg.value.
-   */
-   function PausableDestructibleHasNoEther() public payable {
-     require(msg.value == 0);
-   }
-
-   /**
-    * @dev Disallows direct send by settings a default function without the `payable` flag.
-    */
-   function() external {}
-
-}
-
-
-contract ContributorWhitelist is PausableDestructibleHasNoEther {
-
-  SimpleTGE public TGEContract;
-
-  mapping (address => bool) public whitelist;
-
-  modifier onlyOwnerOrTGE() {
-    require((msg.sender == owner) || ((address(TGEContract) != 0) && (msg.sender == address(TGEContract))));
-    _;
-  }
-
-  function setTGEContract(address _address) onlyOwner external returns(bool) {
-      TGEContract = SimpleTGE(_address);
-      return true;
-  }
-
-  function whitelistAddress(address _address) external onlyOwner returns(bool) {
-    whitelist[_address] = true;
-    return true;
-  }
-
-  function blacklistAddress(address _address) external onlyOwner returns(bool) {
-    whitelist[_address] = false;
-    return true;
-  }
-
-  function bulkWhitelistAddresses(address[] addrs) external onlyOwner returns(bool) {
-    require(addrs.length <= 100);
-    for (uint i=0; i<addrs.length; i++) {
-      whitelist[addrs[i]] = true;
-    }
-    return true;
-  }
-
-  function isWhitelisted(address _address) external onlyOwnerOrTGE view returns(bool) {
-    return whitelist[_address];
-  }
-
-}
 
 
 
@@ -217,11 +98,10 @@ contract SimpleTGE is Ownable {
   // individual cap in wei
   uint256 public individualCapInWei;
 
-  // contract that holds the whitelisted address
-  ContributorWhitelist public whitelist;
 
   uint256 public TRSOffset = 7 days;
 
+  mapping (address => bool) public whitelist;
 
   address[] public contributors;
   struct Contribution {
@@ -237,12 +117,28 @@ contract SimpleTGE is Ownable {
     _;
   }
 
+  modifier isWhitelisted() {
+    require(whitelist[msg.sender]);
+    _;
+  }
 
-  function setWhitelist(address _address) onlyOwner external returns (bool) {
-    whitelist = ContributorWhitelist(_address);
+  function blacklistAddresses(address[] addrs) external onlyOwner returns(bool) {
+    require(addrs.length <= 100);
+    for (uint i=0; i<addrs.length; i++) {
+      require(addrs[i] != address(0));
+      whitelist[addrs[i]] = false;
+    }
     return true;
   }
 
+  function whitelistAddresses(address[] addrs) external onlyOwner returns(bool) {
+    require(addrs.length <= 100);
+    for (uint i=0; i<addrs.length; i++) {
+      require(addrs[i] != address(0));
+      whitelist[addrs[i]] = true;
+    }
+    return true;
+  }
 
 
   /**
@@ -280,13 +176,12 @@ contract SimpleTGE is Ownable {
     //validations
     require(msg.sender != address(0));
     require(msg.value != 0);
-    require(whitelist.isWhitelisted(msg.sender));
     require(weiRaised  + msg.value <= totalCapInWei);
     require(contributions[msg.sender].weiContributed  + msg.value <= individualCapInWei);
 
-
-    if ((contributors.length == 0) || (contributors[0] != msg.sender)) {
-        contributors.push(msg.sender);
+    // if we have not received any WEI from this address until now, then we add this address to contributors list.
+    if (contributions[msg.sender].weiContributed == 0){
+      contributors.push(msg.sender);
     }
 
     contributions[msg.sender].weiContributed = contributions[msg.sender].weiContributed.add(msg.value);
@@ -296,14 +191,12 @@ contract SimpleTGE is Ownable {
     fundsWallet.transfer(msg.value);
   }
 
-  function contributeAndVest() external whilePublicTGEIsActive payable {
-    bool _vestingDecision = true;
-    contribute(_vestingDecision);
+  function contributeAndVest() external whilePublicTGEIsActive isWhitelisted payable {
+    contribute(true);
   }
 
-  function contributeWithoutVesting() public whilePublicTGEIsActive payable {
-    bool _vestingDecision = false;
-    contribute(_vestingDecision);
+  function contributeWithoutVesting() public whilePublicTGEIsActive isWhitelisted payable {
+    contribute(false);
   }
 
   // fallback function can be used to buy tokens
@@ -312,9 +205,11 @@ contract SimpleTGE is Ownable {
   }
   // Vesting logic
   // The following cases are checked for _beneficiary's actions:
-  function vest(bool _vestingDecision) external returns(bool) {
+  function vest(bool _vestingDecision) external isWhitelisted returns(bool) {
     bool existingDecision = contributions[msg.sender].hasVested;
     require(existingDecision != _vestingDecision);
+    require(block.timestamp >= publicTGEStartBlockTimeStamp);
+    require(contributions[msg.sender].weiContributed > 0);
     // Ensure vesting cannot be done once TRS starts
     if (block.timestamp > publicTGEEndBlockTimeStamp) {
       require(block.timestamp.sub(publicTGEEndBlockTimeStamp) <= TRSOffset);
